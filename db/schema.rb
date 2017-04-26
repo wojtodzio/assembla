422f691d12715e4620094cb51fa4daebf34d1dbe
Wojtek Wrona <w.wrona@selleo.com>
2017-04-26 13:37:35 +0200

    Create users

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421092831) do
+ActiveRecord::Schema.define(version: 20170426103614) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.string   "duration"
     t.string   "director"
-    t.integer  "rating"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   null: false
+    t.string   "password",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
